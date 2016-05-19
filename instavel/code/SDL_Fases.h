4fb5f3de467d5527b71aec27aee138520b92fbb4
flaviokc <flavyovynycyus@gmail.com>
2016-05-18 23:44:21 -0300

    Add files via upload

---
+++ instavel/code/SDL_Fases.h
@@ -0,0 +1,12 @@
+#ifndef SDL_FASES_H
+    #define SDL_FASES_H
+
+    #include <SDL.h>
+    #include "bool.h"
+
+    //esse arquivo declara as fases do jogo.
+
+    //fases:
+    void fase1(const int* SCREEN_WIDTH, const int* SCREEN_HEIGHT);
+
+#endif
